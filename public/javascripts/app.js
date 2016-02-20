3415c084c8f0681cd90a4ec79568bdd964845838
Derek Barnhart <derekbarnhart@gmail.com>
2016-02-20 12:01:07 -0800

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+(function(context){
+
+  console.log('Hey got loaded');
+
+  var TESTER = document.getElementById('tester');
+  Plotly.plot( TESTER, [
+    {
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16]
+    }],
+    {
+      margin: { t: 0 } } );
+
+
+}(window);
